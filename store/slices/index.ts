26329449afd28fd3c1dc1a815db940ecaedbce98
ShineShao <xiaoshaoqq@gmail.com>
2022-09-27 19:16:53 +0800

    fix: types

--- store/slices/index.ts
+++ store/slices/index.ts
@@ -1,18 +1,24 @@
+import { Reducer, Slice } from "@reduxjs/toolkit";
+
 import { authSlice } from "./auth";
 
 const combineSlice = {
   auth: authSlice
 };
 
+type CombineSliceKeyType = keyof typeof combineSlice;
+
 // https://jsonplaceholder.typicode.com/users/${state$.value.nextUserId}
 
 // get all reducer
-export const reducers = Object.keys(combineSlice).reduce(
-  (prev: Record<string, any>, key: string) => {
-    prev[key] = combineSlice[key].reducer;
+export const reducers = Object.entries<Slice>(combineSlice).reduce(
+  (prev: Record<CombineSliceKeyType, Reducer>, entry) => {
+    prev[entry[0] as CombineSliceKeyType] =
+      combineSlice[entry[0] as CombineSliceKeyType].reducer;
+
     return prev;
   },
-  {} as Record<string, any>
+  {} as Record<CombineSliceKeyType, Reducer>
 );
 
 export type CombineSliceType = typeof combineSlice;
