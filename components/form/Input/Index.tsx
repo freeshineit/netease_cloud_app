d0a5c580b03820bb77b4ce66a2f07a70190dfee5
ShineShao <xiaoshaoqq@gmail.com>
2022-09-27 19:17:12 +0800

    feat: Input component

---
+++ components/form/Input/Index.tsx
@@ -0,0 +1,10 @@
+import React, {FC} from 'react';
+
+
+export interface InputProps extends NetEase.CommonComponentProps {}
+
+const Input:FC<InputProps> = () => {
+	return <input />
+}
+
+export default Input
